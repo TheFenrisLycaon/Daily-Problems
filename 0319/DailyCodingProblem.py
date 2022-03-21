0e13a90a005538f8ccd7caacf54d889e2f05a42e
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-21 10:22:39 +0530

    Added 0319

---
+++ 0319/DailyCodingProblem.py
@@ -0,0 +1,14 @@
+"""
+Given an integer k and a string s,
+find the length of the longest substring that contains at most k distinct characters.
+
+For example, given s = "abcba" and k = 2,
+the longest substring with k distinct characters is "bcb".
+"""
+from typing import *
+
+def foo():
+    pass
+
+for i in range(int(input())):
+    print(foo())
