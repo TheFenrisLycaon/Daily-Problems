07897f5fed4ca7ef21f7b95eaa9b7727194fedcd
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-22 10:08:55 +0530

    Added 0322

---
+++ 0322/DailyCodingProblem.py
@@ -0,0 +1,11 @@
+"""
+Given a stream of elements too large to store in memory,
+pick a random element from the stream with uniform probability.
+"""
+from typing import *
+
+def foo():
+    pass
+
+for i in range(int(input())):
+    print(foo())
