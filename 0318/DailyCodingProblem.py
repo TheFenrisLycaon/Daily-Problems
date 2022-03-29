7149556219a98c1601c4e9b9f44f6e26f6af0d53
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-29 07:28:28 +0530

    Added 0318 DCP

--- 0318/DailyCodingProblem.py
+++ 0318/DailyCodingProblem.py
@@ -19,9 +19,19 @@ For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
 from typing import *
 
 
-def foo():
-    pass
+def climbStairs(n: int) -> int:
+
+    if n == 0 or n == 1:
+        return 1
+
+    ff = 1
+    sf = 2
+    for _ in range(3, n+1):
+        nf = ff + sf
+        ff, sf = sf, nf
+
+    return sf
 
 
 for i in range(int(input())):
-    print(foo())
+    print(climbStairs(int(input())))
