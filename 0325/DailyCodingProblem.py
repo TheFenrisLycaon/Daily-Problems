24b6d510f9c5b996c912ca2f3726910ce572f4e6
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-29 09:02:29 +0530

    Added 0325 DCP

--- 0325/DailyCodingProblem.py
+++ 0325/DailyCodingProblem.py
@@ -8,8 +8,27 @@ return the minimum cost which achieves this goal.
 """
 from typing import *
 
-def foo():
-    pass
+
+def paint(arr: List[List[int]]) -> int:
+    results = []
+    helper(arr, results, 0, -1, 0)
+    return min(results)
+
+
+def helper(arr, results, curr_house, prev_color, curr_cost):
+    if curr_house == len(arr):
+        results.append(curr_cost)
+        return
+    for i in range(0, len(arr[0])):
+        if i != prev_color:
+            helper(
+                arr,
+                results,
+                curr_house + 1,
+                i,
+                arr[curr_house][i] + curr_cost,
+            )
+
 
 for i in range(int(input())):
-    print(foo())
+    print(paint([[1, 2, 3, 4], [1, 2, 1, 0], [6, 1, 1, 5], [2, 3, 5, 5]]))
