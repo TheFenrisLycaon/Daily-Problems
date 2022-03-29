fa77acf457f9fe819084d198354fdb1ae3c8aab1
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-29 07:46:14 +0530

    Added 0321 DCP

--- 0321/DailyCodingProblem.py
+++ 0321/DailyCodingProblem.py
@@ -3,9 +3,18 @@ Given a stream of elements too large to store in memory,
 pick a random element from the stream with uniform probability.
 """
 from typing import *
+from random import randint
 
-def foo():
-    pass
+
+def pickRandom(stream: List[int]) -> int:
+    res = None
+    for i, e in enumerate(stream):
+        if i == 0:
+            res = e
+        elif randint(1, i + 1) == 1:
+            res = e
+    return res
+
 
 for i in range(int(input())):
-    print(foo())
+    print(pickRandom(list(map(int, input().split()))))
