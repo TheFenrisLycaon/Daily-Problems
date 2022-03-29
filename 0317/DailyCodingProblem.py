c323976a94de91299c98b554f31eb177b997fc7c
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-29 07:28:33 +0530

    Added 0317 DCP

--- 0317/DailyCodingProblem.py
+++ 0317/DailyCodingProblem.py
@@ -1,6 +1,6 @@
 """
-Implement an autocomplete system. T
-hat is, given a query string s and a set of all possible query strings,
+Implement an autocomplete system.
+That is, given a query string s and a set of all possible query strings,
 return all strings in the set that have s as a prefix.
 
 For example, given the query string de and the set of strings [dog, deer, deal],
@@ -9,11 +9,12 @@ return [deer, deal].
 Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
 """
 from typing import *
+import re
 
 
-def foo():
-    pass
+def autocomplete(s: str, dictionary: List[str]) -> List[str]:
+    return [x for x in dictionary if re.compile("^" + s).match(x)]
 
 
 for i in range(int(input())):
-    print(foo())
+    print(autocomplete(input(), input().split()))
