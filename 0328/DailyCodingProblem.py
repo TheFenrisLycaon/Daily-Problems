297857507935fd38dc783fc3927c805ad3afada1
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-29 10:00:03 +0530

    Added 0328 DCP

--- 0328/DailyCodingProblem.py
+++ 0328/DailyCodingProblem.py
@@ -20,9 +20,16 @@ return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
 from typing import *
 
 
-def foo():
-    pass
+def search(string: str, words: List[str]) -> List[str]:
+    """
+    Given a dictionary of words and a string made up of those words (no spaces),
+    return the original sentence in a list.
+    """
+    for word in words:
+        if string.startswith(word):
+            return [word] + search(string[len(word) :], words)
+    return []
 
 
 for i in range(int(input())):
-    print(foo())
+    print(search(input(), input().split()))
